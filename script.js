ff64ea4436a120cdd71da6c0663a73c5eb5f2627
Barutta02 <88086997+Barutta02@users.noreply.github.com>
2022-09-10 17:45:06 +0200

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+function generaCodice() {
+  document.getElementById("viewQrCode").innerHTML = "";
+  nome = document.getElementById("inputNome").value;
+  cognome = document.getElementById("inputCognome").value;
+  document.getElementById("copiaApp").style.display = "block";
+  var qrc = new QRCode(document.getElementById("viewQrCode"), {
+    text: nome + " " + cognome + " " + generaSeqUnivoca(),
+    width: 200,
+    height: 200,
+    colorDark: "#000000",
+  });
+}
+
+function generaSeqUnivoca() {
+  var seq = "";
+  for (var i = 0; i < 6; i++) {
+    seq += Math.floor(Math.random() * 10);
+  }
+
+  document.getElementById("sequenzaUnivoca").value = seq;
+  return seq;
+}
+
+document.querySelector("#copiaApp").onclick = function () {
+  // selezione del contenuto
+  document.querySelector("#sequenzaUnivoca").select();
+  // copia negli appunti
+  document.execCommand("copy");
+};
